9e9a95c3ac4e4e57d88f20b90326f2b39866e8ff
Daniel Melonari <daniel.melonari@MBPdemabookpro2.SEDE.SOVIS>
2023-08-02 10:08:25 -0300

    Trabalhando com records

---
+++ lib/core/native_records.dart
@@ -0,0 +1,57 @@
+abstract class IUseCase<R, P> {
+  Future<(Failure?, R?)> call(P params);
+}
+
+abstract class Failure {
+  final String message;
+
+  const Failure({
+    required this.message
+  });
+}
+
+class ExampleFailure extends Failure {
+  const ExampleFailure({
+    required super.message
+  });
+}
+
+class NativeRecords<C extends IUseCase, P> {
+  final C instance;
+  final P params;
+  
+  const NativeRecords({
+    required this.instance,
+    required this.params
+  });
+  
+  void fold<R>(Function(Failure failure) failure, Function(R result) result) async {
+    final (fail, ok) = await instance(params);
+    if (fail != null) {
+      failure(fail);
+      return;
+    }
+    
+    if (ok != null) {
+      result(ok);
+    }
+  }
+}
+
+class NoParams {}
+
+class GetVersion extends IUseCase<String?, NoParams> {
+  @override
+  Future<(Failure?, String?)> call(NoParams params) async {
+    return (null, "V1.5");
+  }
+}
+
+void main() async {
+  final result = NativeRecords<GetVersion, NoParams>(
+    instance: GetVersion(),
+    params: NoParams()
+  );
+
+  result.fold((failure) => print(failure.message), (result) => print(result));
+}
